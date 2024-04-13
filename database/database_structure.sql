1a7a521b6093e46a1ad55f7dd53619bf9671f13e
Mido <124526040+midosvt@users.noreply.github.com>
2024-04-13 15:02:29 +0000

    Database structure

---
+++ database/database_structure.sql
@@ -0,0 +1,13 @@
+CREATE TABLE players (
+    ID INT PRIMARY KEY,
+    Name VARCHAR(255),
+    Hash VARCHAR(255),
+    Skin INT,
+    Score INT,
+    Kills INT,
+    Deaths INT,
+    x_pos FLOAT,
+    y_pos FLOAT,
+    z_pos FLOAT,
+    angle_pos FLOAT
+);
